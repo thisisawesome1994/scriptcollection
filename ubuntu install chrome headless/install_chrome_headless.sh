ee82d97afe3a2809998a0728ff5177ac5a149911
thisisawesome1994 <58063460+thisisawesome1994@users.noreply.github.com>
2021-07-14 06:02:34 +0200

    Add files via upload

---
+++ ubuntu install chrome headless/install_chrome_headless.sh
@@ -0,0 +1,4 @@
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+chmod u+rwx google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb
+apt install -f
